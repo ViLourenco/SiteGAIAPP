2a9cc0c84c3b5a25cb6ae7279dcc50038d745a6e
ViLourenco <vinicius.q.lourenco@gmail.com>
2014-02-06 12:19:05 -0200

    Arquivos do site versões inglês e português

---
+++ en/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>God and I</title>
+    
+    <meta property="og:title" content="God and I - App"/>
+    <meta property="og:url" content="http://godandiapp.com/en"/>
+	<meta property="og:image" content="http://godandiapp.com/site/images/logoiphonetag.png"/>
+	<meta property="og:site_name" content="God and I - App"/>
+	<meta property="og:description" content="God and I - A quick and easy way to follow devotional of various themes, authors and different series!. An app to help you in your walk with God."/>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<link href="site/css/iefix.css" type="text/css" rel="stylesheet" />
+	<![endif]-->
+
+	<link href="http://www.godandiapp.com/site/css/main.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+	<div id="home"></div>
+
+	<header>
+		<div class="inner small">
+
+			<figure class="logo">
+				<a href="#" style="display:none;cursor:default;"><i class=""></i>God and I App</a>
+                <img src="http://godandiapp.com/site/images/logo.png" />
+			</figure>
+
+			<nav id="nav">
+				<ul class="nav">
+					<li class="current" style="display:none;"><a href="#home">Home</a></li>
+                    <li class="current"><a href="#" style="cursor:default"><i class=""></i>Launch planned for 2014</a></li>		
+				</ul>
+			</nav>
+
+		</div>
+	</header>
+
+	<section class="intro">
+		<div class="inner small">
+
+			<div class="cage">
+
+			<div class="image">
+				<img src="http://godandiapp.com/site/images/phone.png" alt="" style="margin-top: 35px;" />
+			</div>				
+
+			<div class="text">
+				<h2 style="font-family:'Open Sans', sans-serif;font-weight:600;">We're almost done...</h2>
+
+				<p style="font-family:'Open Sans', sans-serif;font-weight:300;">
+				God and I - A quick and easy way to follow devotional of various themes, authors and different series!<br>
+                <p>
+                <p style="font-family:'Open Sans', sans-serif;font-weight:300;">
+                An app to help you in your walk with God..
+				</p>
+
+
+<p><iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fgodandiapp&amp;width=500&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=334632413343481" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:80px;margin-bottom:-30px;" allowTransparency="true"></iframe></p>				
+
+<?php //if(strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') || strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || strstr($_SERVER['HTTP_USER_AGENT'],'Android') ) { 
+			//	$customcss = "";
+			//}else{
+			//	$customcss = "bottom:-115px;";
+			//}
+			?>
+
+				<a href="#" class="apple" style="display:block;cursor:default;<?php //echo $customcss; ?>"><i class="icon-apple"></i></i> <span>Soon on</span> <br> App Store</a>
+				<a href="#" class="android" style="display:block;cursor:default;<?php //echo $customcss; ?>"><i class="icon-android"></i> <span>Soon on</span> <br> Play Store</a>
+			</div>
+
+			</div>			
+
+		</div>
+	</section>
+
+<section id="about" class="row description light" style="display:none;">
+		<div class="inner small">
+
+			<div class="column two-three">
+
+				<h2>Lorem ipsum dolor sit amet</h2>
+
+				<p>Seu momento de devoção com Deus não será mais o mesmo.<br>
+				<br>
+				God and I App é uma forma rápida e fácil de seguir os devocionais de diversos temas, autores e séries diferentes.<br>
+				<br>
+				Você pode compartilhar no Facebook e Twitter as meditações e autores que você está seguindo e a cada nova devocional criada você poderá será notificado de acordo com as suas preferências.<br>
+				<br>
+				God and I é um aplicativo para ajudar você em sua caminhada com Deus.<br></p>
+			</div>
+
+			<div class="column one-three last">
+				<img src="http://godandiapp.com/site/images/tablet.png" alt="" />
+			</div>
+
+		</div>
+	</section>		
+
+	<section class="row signup colored-dark" style="display:none;">
+		<div class="inner small">
+
+			<h3>Receba nosso aviso de lançamento!</h3>
+			<input type="text" placeholder="Digite aqui seu email"><input type="submit">
+
+		</div>
+	</section>	
+
+	<footer class="row footer extra-dark">
+		<div class="inner small">
+
+			<div class="column three">
+				<p><strong>Team</strong><br>
+				Ana Narciso<br>
+				Sérgio Freire<br>
+				Vinícius Caltran<br>
+				Vinícius Lourenço</p>
+			</div>
+
+			<div class="column three">
+				<p><strong>About</strong><br>
+				Christian brazilian young people who want to be relevant using the skills God has given us.
+                </p>
+			</div>
+
+			<div class="column three last">
+				<p>Copyright 2013 © All rights reserved - God and I</p>
+
+				<a class="instagram" href="mailto:contato@godandiapp.com" ><i class="icon-envelope"></i></a>
+				<a class="twitter" href="#"><i class="icon-twitter"></i></a>
+				<a class="facebook" href="https://www.facebook.com/pages/God-and-I/459039800882615" target="_blank"><i class="icon-facebook"></i></a>
+			</div>
+
+		</div>
+	</footer>				
+
+</body>
+
+
+	<!-- load jquery scripts -->
+	<script src="http://godandiapp.com/site/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/modernizr-2.6.1.min.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/lightbox.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/scrollto.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/contentslider.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/backstretch.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/localscroll.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/nav.js" type="text/javascript"></script>
+	<script src="http://godandiapp.com/site/js/onenav.js" type="text/javascript"></script>
+
+
+	<script type="text/javascript">
+	$(document).ready(function(){
+
+	 	 $('.image-link').magnificPopup({
+	 	 	type:'image',
+			  // Delay in milliseconds before popup is removed
+			  removalDelay: 300,
+
+			  // Class that is added to popup wrapper and background
+			  // make it unique to apply your CSS animations just to this exact popup
+			  mainClass: 'mfp-fade',
+			  fixedContentPos: false
+	 	 }); 
+
+		$(".intro").backstretch("http://godandiapp.com/site/images/sliderBg.jpg");
+
+		$('.contentslider').bxSlider({
+		  adaptiveHeight: true,
+		  mode: 'fade',
+		  pager: false,
+		  controls: true,
+		  nextText: '<i class="icon-chevron-right"></i>',
+		  prevText: '<i class="icon-chevron-left"></i>'
+		});
+
+		$('.nav').onePageNav();	
+		
+		var nav = responsiveNav("#nav", { // Selector
+		  animate: true, // Boolean: Use CSS3 transitions, true or false
+		  transition: 400, // Integer: Speed of the transition, in milliseconds
+		  label: "<i class='icon-reorder'>​</i>​", // String: Label for the navigation toggle
+		  insert: "after", // String: Insert the toggle before or after the navigation
+		  customToggle: "", // Selector: Specify the ID of a custom toggle
+		  openPos: "relative", // String: Position of the opened nav, relative or static
+		  jsClass: "js", // String: 'JS enabled' class which is added to <html> el
+		  init: function(){}, // Function: Init callback
+		  open: function(){}, // Function: Open callback
+		  close: function(){} // Function: Close callback
+		});
+
+	});
+	</script>
+
+</html>
---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>God and I</title>
+    
+    <meta property="og:title" content="God and I - App"/>
+    <meta property="og:url" content="http://www.godandiapp.com"/>
+	<meta property="og:image" content="http://godandiapp.com/site/images/logoiphonetag.png"/>
+	<meta property="og:site_name" content="God and I - App"/>
+	<meta property="og:description" content="God and I é uma forma rápida e fácil de seguir os devocionais de diversos temas, autores e séries diferentes. É um aplicativo para ajudar você em sua caminhada com Deus."/>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<link href="site/css/iefix.css" type="text/css" rel="stylesheet" />
+	<![endif]-->
+
+	<link href="site/css/main.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+	<div id="home"></div>
+
+	<header>
+		<div class="inner small">
+
+			<figure class="logo">
+				<a href="#" style="display:none;cursor:default;"><i class=""></i>God and I App</a>
+                <img src="http://godandiapp.com/site/images/logo.png" />
+			</figure>
+
+			<nav id="nav">
+				<ul class="nav">
+					<li class="current" style="display:none;"><a href="#home">Home</a></li>
+                    <li class="current"><a href="#" style="cursor:default"><i class=""></i>Lançamento previsto para 2014</a></li>		
+				</ul>
+			</nav>
+
+		</div>
+	</header>
+
+	<section class="intro">
+		<div class="inner small">
+
+			<div class="cage">
+
+			<div class="image">
+				<img src="site/images/phone.png" alt="" style="margin-top: 35px;" />
+			</div>				
+
+			<div class="text">
+				<h2 style="font-family:'Open Sans', sans-serif;font-weight:600;">Estamos quase terminando...</h2>
+
+				<p style="font-family:'Open Sans', sans-serif;font-weight:300;">
+				God and I é uma forma rápida e fácil de seguir os devocionais<br> de diversos temas, autores e séries diferentes.
+                <p>
+                <p style="font-family:'Open Sans', sans-serif;font-weight:300;">
+                Um aplicativo para ajudar você em sua caminhada com Deus.
+				</p>
+
+<p><iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fgodandiapp&amp;width=500&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=334632413343481" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:500px; height:80px;margin-bottom:-30px;" allowTransparency="true"></iframe></p>
+
+
+<?php //if(strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') || strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || strstr($_SERVER['HTTP_USER_AGENT'],'Android') ) { 
+			//	$customcss = "";
+			//}else{
+			//	$customcss = "bottom:-115px;";
+			//}
+			?>
+
+				<a href="#" class="apple" style="display:block;cursor:default;<?php //echo $customcss; ?>"><i class="icon-apple"></i></i> <span>Em breve na</span> <br> App Store</a>
+				<a href="#" class="android" style="display:block;cursor:default;<?php //echo $customcss; ?>"><i class="icon-android"></i> <span>Em breve na</span> <br> Play Store</a>
+
+
+			</div>
+
+			</div>			
+		</div>
+	</section>
+
+<section id="about" class="row description light" style="display:none;">
+		<div class="inner small">
+
+			<div class="column two-three">
+
+				<h2>Lorem ipsum dolor sit amet</h2>
+
+				<p>Seu momento de devoção com Deus não será mais o mesmo.<br>
+				<br>
+				God and I App é uma forma rápida e fácil de seguir os devocionais de diversos temas, autores e séries diferentes.<br>
+				<br>
+				Você pode compartilhar no Facebook e Twitter as meditações e autores que você está seguindo e a cada nova devocional criada você poderá será notificado de acordo com as suas preferências.<br>
+				<br>
+				God and I é um aplicativo para ajudar você em sua caminhada com Deus.<br></p>
+			</div>
+
+			<div class="column one-three last">
+				<img src="site/images/tablet.png" alt="" />
+			</div>
+
+		</div>
+	</section>		
+
+	<section class="row signup colored-dark" style="display:none;">
+		<div class="inner small">
+
+			<h3>Receba nosso aviso de lançamento!</h3>
+			<input type="text" placeholder="Digite aqui seu email"><input type="submit">
+
+		</div>
+	</section>	
+
+	<footer class="row footer extra-dark">
+		<div class="inner small">
+
+			<div class="column three">
+				<p><strong>Equipe</strong><br>
+				Ana Narciso<br>
+				Sérgio Freire<br>
+				Vinícius Caltran<br>
+				Vinícius Lourenço</p>
+			</div>
+
+			<div class="column three">
+				<p><strong>Quem Somos</strong><br>
+				Jovens cristãos que queremos ser relevantes usando as habilidades que Deus nos deu.
+                </p>
+			</div>
+
+			<div class="column three last">
+				<p>Copyright 2013 © Todos os direitos reservados - God and I</p>
+
+				<a class="instagram" href="mailto:contato@godandiapp.com" ><i class="icon-envelope"></i></a>
+				<a class="twitter" href="#"><i class="icon-twitter"></i></a>
+				<a class="facebook" href="https://www.facebook.com/pages/God-and-I/459039800882615" target="_blank"><i class="icon-facebook"></i></a>
+			</div>
+
+		</div>
+	</footer>				
+
+</body>
+
+
+	<!-- load jquery scripts -->
+	<script src="site/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+	<script src="site/js/modernizr-2.6.1.min.js" type="text/javascript"></script>
+	<script src="site/js/lightbox.js" type="text/javascript"></script>
+	<script src="site/js/scrollto.js" type="text/javascript"></script>
+	<script src="site/js/contentslider.js" type="text/javascript"></script>
+	<script src="site/js/backstretch.js" type="text/javascript"></script>
+	<script src="site/js/localscroll.js" type="text/javascript"></script>
+	<script src="site/js/nav.js" type="text/javascript"></script>
+	<script src="site/js/onenav.js" type="text/javascript"></script>
+
+
+	<script type="text/javascript">
+	$(document).ready(function(){
+
+	 	 $('.image-link').magnificPopup({
+	 	 	type:'image',
+			  // Delay in milliseconds before popup is removed
+			  removalDelay: 300,
+
+			  // Class that is added to popup wrapper and background
+			  // make it unique to apply your CSS animations just to this exact popup
+			  mainClass: 'mfp-fade',
+			  fixedContentPos: false
+	 	 }); 
+
+		$(".intro").backstretch("site/images/sliderBg.jpg");
+
+		$('.contentslider').bxSlider({
+		  adaptiveHeight: true,
+		  mode: 'fade',
+		  pager: false,
+		  controls: true,
+		  nextText: '<i class="icon-chevron-right"></i>',
+		  prevText: '<i class="icon-chevron-left"></i>'
+		});
+
+		$('.nav').onePageNav();	
+		
+		var nav = responsiveNav("#nav", { // Selector
+		  animate: true, // Boolean: Use CSS3 transitions, true or false
+		  transition: 400, // Integer: Speed of the transition, in milliseconds
+		  label: "<i class='icon-reorder'>​</i>​", // String: Label for the navigation toggle
+		  insert: "after", // String: Insert the toggle before or after the navigation
+		  customToggle: "", // Selector: Specify the ID of a custom toggle
+		  openPos: "relative", // String: Position of the opened nav, relative or static
+		  jsClass: "js", // String: 'JS enabled' class which is added to <html> el
+		  init: function(){}, // Function: Init callback
+		  open: function(){}, // Function: Open callback
+		  close: function(){} // Function: Close callback
+		});
+
+	});
+	</script>
+
+</html>
